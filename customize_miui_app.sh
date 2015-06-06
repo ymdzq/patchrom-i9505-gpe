facd044b008bfc019a6f806849745b5b869f8aa0
ymdzq <233900743@qq.com>
2015-06-06 21:39:15 +0800

    Upload Updates

---
+++ customize_miui_app.sh
@@ -0,0 +1,66 @@
+#/bin/bash
+
+XMLMERGYTOOL=$PORT_ROOT/tools/ResValuesModify/jar/ResValuesModify
+GIT_APPLY=$PORT_ROOT/tools/git.apply
+
+curdir=`pwd`
+
+function applyPatch () {
+    for patch in `find $1 -name *.patch`
+    do
+        cd out
+        $GIT_APPLY ../$patch
+        cd ..
+        for rej in `find $2 -name *.rej`
+        do
+            echo "Patch $patch fail"
+            exit 1
+        done
+    done
+}
+
+function appendSmaliPart() {
+    for file in `find $1/smali -name *.part`
+    do
+		filepath=`dirname $file`
+		filename=`basename $file .part`
+		dstfile="out/$filepath/$filename"
+        cat $file >> $dstfile
+    done
+}
+
+function mergyXmlPart() {
+	for file in `find $1/res -name *.xml.part`
+	do
+		src=`dirname $file`
+		dst=${src/$1/$2}
+		$XMLMERGYTOOL $src $dst
+	done
+}
+
+if [ $1 = "DeskClock" ];then
+	applyPatch $1 $2
+fi
+
+if [ $1 = "MiuiFramework" ];then
+    cp $1/jflte.xml $2/assets/device_features/
+    cp $1/jgedlte.xml $2/assets/device_features/
+	applyPatch $1 $2
+fi
+
+if [ $1 = "MiuiHome" ];then
+    $XMLMERGYTOOL $1/res/values $2/res/values
+fi
+
+if [ $1 = "Music" ];then
+	applyPatch $1 $2
+fi
+
+if [ $1 = "SecurityCenter" ];then
+	applyPatch $1 $2
+fi
+
+if [ $1 = "TeleService" ];then
+    $XMLMERGYTOOL $1/res/values $2/res/values
+	applyPatch $1 $2
+fi
